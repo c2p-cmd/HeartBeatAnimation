02320fc915a925c9411aa879ae9b10877c5ad3b6
Sharan Thakur <sharandt19@live.com>
2024-02-13 16:53:09 +0530

    Initial Commit

---
+++ HeartBeatAnimation/HeartBeatAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartBeatAnimationApp.swift
+//  HeartBeatAnimation
+//
+//  Created by Sharan Thakur on 13/02/24.
+//
+
+import SwiftUI
+
+@main
+struct HeartBeatAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
